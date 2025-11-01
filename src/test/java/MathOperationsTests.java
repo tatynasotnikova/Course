1c6b3bf6144b10f5c86fdbfaacf372e23230cdb4
tatynasotnikova <tanya_sotnikova_97@mail.ru>
2025-11-01 23:48:57 +0300

    lesson - 7: MathOperationsTests.

--- src/test/java/MathOperationsTests.java
+++ src/test/java/MathOperationsTests.java
@@ -1,70 +1,64 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.assertNotEquals;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class MathOperationsTests {
     @Test
-    @DisplayName("Тестирование вычисления факториала")
     void factorialTest() {
         assertEquals(1,MathOperations.factorial(1));
         assertEquals(6,MathOperations.factorial(3));
-        assertNotEquals(721, MathOperations.factorial(6));
-        IllegalArgumentException positiveNumbersException = assertThrows(IllegalArgumentException.class, () -> {
-            MathOperations.factorial(-6);
-        });
-        assertEquals("Факториал определен только для неотрицательных чисел", positiveNumbersException.getMessage());
+        assertNotEquals(MathOperations.factorial(6), 721);
+    }
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void invalidInputFactorial() {
+        MathOperations.factorial(-9);
     }
 
     @Test
-    @DisplayName("Тестирование вычисления площади треугольника")
     void triangleAreaTest() {
-        assertEquals(6, MathOperations.triangleArea(3, 4, 5));
-        assertNotEquals(56, MathOperations.triangleArea(3, 4, 5));
-        IllegalArgumentException positiveNumbersException = assertThrows(IllegalArgumentException.class, () -> { MathOperations.triangleArea(6, 0, 0); });
-        assertEquals("Стороны треугольника должны быть положительными числами", positiveNumbersException.getMessage());
-        IllegalArgumentException notTriangleException = assertThrows(IllegalArgumentException.class, () -> {
-            MathOperations.triangleArea(10, 2, 2);
-        });
-        assertEquals("Стороны не образуют треугольник", notTriangleException.getMessage());
+        assertEquals(6.0, MathOperations.triangleArea(3, 4, 5));
+        assertNotEquals(MathOperations.triangleArea(3, 4, 5), 56);
+    }
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void invalidInputTriangleArea() {
+        MathOperations.triangleArea(6, 0, 0);
+        MathOperations.triangleArea(10, 2, 2);
     }
 
     @Test
-    @DisplayName("Тестирование сложения")
     void addTest() {
         assertEquals(4, MathOperations.add(2,2));
-        assertNotEquals(25, MathOperations.add(6, 8));
+        assertNotEquals(MathOperations.add(6, 8), 25);
     }
     @Test
-    @DisplayName("Тестирование вычитания")
     void subtractTest() {
         assertEquals(10, MathOperations.subtract(15, 5));
-        assertNotEquals(5, MathOperations.subtract(10, 8));
+        assertNotEquals(MathOperations.subtract(10, 8), 5);
     }
 
     @Test
-    @DisplayName("Тестирование умножения")
     void multiplyTest() {
         assertEquals(18, MathOperations.multiply(2, 9));
-        assertNotEquals(28, MathOperations.multiply(6, 4));
+        assertNotEquals(MathOperations.multiply(6, 4), 28);
     }
 
     @Test
-    @DisplayName("Тестирование деления")
     void divideTest() {
-        assertEquals(2, MathOperations.divide(4, 2));
-        ArithmeticException thrown = assertThrows(ArithmeticException.class, () -> { MathOperations.divide(6, 0); });
-        assertEquals("Деление на ноль невозможно", thrown.getMessage());
+        assertEquals(2.0, MathOperations.divide(4, 2));
+    }
+    @Test(expectedExceptions = ArithmeticException.class)
+    public void invalidInputDivide() {
+        MathOperations.divide(6, 0);
     }
 
     @Test
-    @DisplayName("Тестирование сравнения")
     void compareWithDescriptionTest() {
         assertEquals("1 меньше 5", MathOperations.compareWithDescription(1, 5));
         assertEquals("6 равно 6", MathOperations.compareWithDescription(6, 6));
         assertEquals("8 больше 4", MathOperations.compareWithDescription(8, 4));
-        assertNotEquals("17 меньше 7", MathOperations.compareWithDescription(17, 7));
-        assertNotEquals("8 не равно 8", MathOperations.compareWithDescription(8, 8));
-        assertNotEquals("2 больше 12", MathOperations.compareWithDescription(2, 12));
+        assertNotEquals(MathOperations.compareWithDescription(17, 7), "17 меньше 7");
+        assertNotEquals(MathOperations.compareWithDescription(8, 8), "8 не равно 8");
+        assertNotEquals(MathOperations.compareWithDescription(2, 12), "2 больше 12");
     }
 }
